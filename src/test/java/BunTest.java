281e3da3dc87abe4d30247e890f439c03e52a242
Alexander Babkin <sann4obabkin@yandex.ru>
2025-02-02 18:24:31 +0300

    second commit

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+import praktikum.Database;
 
+@RunWith(Parameterized.class)
 public class BunTest {
+
+    private String bunName;
+    private float bunPrice;
+
+    static Database database = new Database();
+
+    static String name1 = database.availableBuns().get(0).name;
+    static String name2 = database.availableBuns().get(1).name;
+    static String name3 = database.availableBuns().get(2).name;
+
+    static float price1 = database.availableBuns().get(0).price;
+    static float price2 = database.availableBuns().get(1).price;
+    static float price3 = database.availableBuns().get(2).price;
+
+    public BunTest(String bunName, float bunPrice) {
+        this.bunName = bunName;
+        this.bunPrice = bunPrice;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBunData() {
+        return new Object[][] {
+                {name1, price1},
+                {name2, price2},
+                {name3, price3}
+        };
+    }
+
+    @Test
+    public void getBunNameTest() {
+        Bun bun = new Bun(bunName, bunPrice);
+        String actual = bun.getName();
+        Assert.assertEquals("Неверное название булки", bunName, actual);
+    }
+
+    @Test
+    public void getBunPriceTest() {
+        Bun bun = new Bun(bunName, bunPrice);
+        float actual = bun.getPrice();
+        Assert.assertEquals("Неверная цена булки", bunPrice, actual, 0);
+    }
 }
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,4 +1,73 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Database;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
+
+    private IngredientType ingredientType;
+    private String ingredientName;
+    private float ingredientPrice;
+
+    static Database database = new Database();
+    static IngredientType type1 = IngredientType.SAUCE;
+    static IngredientType type2 = IngredientType.FILLING;
+
+    static String name1 = database.availableIngredients().get(0).name;
+    static String name2 = database.availableIngredients().get(1).name;
+    static String name3 = database.availableIngredients().get(2).name;
+    static String name4 = database.availableIngredients().get(3).name;
+    static String name5 = database.availableIngredients().get(4).name;
+    static String name6 = database.availableIngredients().get(5).name;
+
+    static float price1 = database.availableIngredients().get(0).price;
+    static float price2 = database.availableIngredients().get(1).price;
+    static float price3 = database.availableIngredients().get(2).price;
+    static float price4 = database.availableIngredients().get(3).price;
+    static float price5 = database.availableIngredients().get(4).price;
+    static float price6 = database.availableIngredients().get(5).price;
+
+
+    public IngredientTest(IngredientType ingredientType, String ingredientName, float ingredientPrice) {
+        this.ingredientType = ingredientType;
+        this.ingredientName = ingredientName;
+        this.ingredientPrice = ingredientPrice;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getIngredientData() {
+        return new Object[][] {
+                {type1, name1, price1},
+                {type1, name2, price2},
+                {type1, name3, price3},
+                {type2, name4, price4},
+                {type2, name5, price5},
+                {type2, name6, price6}
+        };
+    }
+
+    @Test
+    public void getIngredientPriceTest() {
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        float actual = ingredient.getPrice();
+        Assert.assertEquals("Неверная цена ингредиента", ingredientPrice, actual, 0);
+    }
+
+    @Test
+    public void getIngredientNameTest() {
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        String actual = ingredient.getName();
+        Assert.assertEquals("Неверное наименование ингредиента", ingredientName, actual);
+    }
+
+    @Test
+    public void getIngredientTypeTest() {
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        IngredientType actual = ingredient.getType();
+        Assert.assertEquals("Неверный тип ингредиента", ingredientType, actual);
+    }
 }
